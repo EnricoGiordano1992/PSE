2174fb044038c30faf72afd9b4fdbf9e33e1ace4
EnricoGiordano1992 <enricogiordano1992@gmail.com>
2014-11-02 17:47:16 +0100

    finito datapath

--- ES1/include/mul_RTL.hh
+++ ES1/include/mul_RTL.hh
@@ -5,42 +5,48 @@
 
 SC_MODULE(mul_RTL){
 
-//flag di input per sapere se sono pronti i numeri da moltiplicare
-  sc_in<sc_uint<1> >    isready;
+	//flag di input per sapere se sono pronti i numeri da moltiplicare
+	sc_in<sc_uint<1> >    isready;
 
-//numeri di input da moltiplicare
-  sc_in<sc_lv<64> >   	number_a;
-  sc_in<sc_lv<64> >  	number_b;
+	//numeri di input da moltiplicare
+	sc_in<sc_lv<64> >   	number_a;
+	sc_in<sc_lv<64> >  	number_b;
 
-//flag di output per sapere quando e' pronto l'output
-  sc_out<sc_uint<1> >   result_isready;
+	//flag di output per sapere quando e' pronto l'output
+	sc_out<sc_uint<1> >   result_isready;
 
-  sc_out<sc_lv<64> >  		result;
-  sc_in<bool>           	reset;
-  sc_in< sc_dt::sc_logic > 	clk;
-
-  typedef enum {SR, S0, S0I, S1, S2, S3, S4, S5, S6, S7, S8, S9} STATES;
-
-  sc_signal<STATES> STATUS, NEXT_STATUS;
-
-  sc_signal<sc_lv<64> >   	vcl_number_a;
-  sc_signal<sc_lv<64> >  	vcl_number_b;
-  sc_signal<sc_lv<64> >  	vcl_result;
+	sc_out<sc_lv<64> >  		result;
 
 
-  void elaborate_mul(void);
-  void elaborate_mul_FSM(void);
+	sc_in<bool>           		reset;
+	sc_in< sc_dt::sc_logic > 	clk;
 
-  SC_CTOR(mul_RTL){
-    SC_METHOD(elaborate_mul_FSM);
-    sensitive << reset.neg();
-    sensitive << clk.pos();
+	typedef enum {SR, S0, S0I, S1, S2, S3, S4, S5, S6, S7, S8, S9} STATES;
 
-     
+	sc_signal<STATES> STATUS, NEXT_STATUS;
 
-    SC_METHOD(elaborate_mul);
-    sensitive << STATUS << isready << number_a << number_b;
-  };
+	sc_signal<sc_lv<64> >   	vcl_number_a;
+	sc_signal<sc_lv<64> >  		vcl_number_b;
+	sc_signal<sc_lv<64> >  		vcl_result;
+
+	//overflow?
+	sc_signal<bool>				error;
+	//normalizzato?
+	sc_signal<bool >		 normalizzato;
+
+	void elaborate_mul(void);
+	void elaborate_mul_FSM(void);
+
+	SC_CTOR(mul_RTL){
+		SC_METHOD(elaborate_mul_FSM);
+		sensitive << reset.neg();
+		sensitive << clk.pos();
+
+
+
+		SC_METHOD(elaborate_mul);
+		sensitive << STATUS << isready << number_a << number_b;
+	};
 };
 
 #endif
--- ES1/src/mul_RTL.cc
+++ ES1/src/mul_RTL.cc
@@ -4,12 +4,17 @@ void mul_RTL :: elaborate_mul_FSM(void){
 
 	sc_lv<11> exp1;
 	sc_lv<53> mantissa1;
+	sc_lv<1>  sign1;
 
 	sc_lv<11> exp2;
 	sc_lv<53> mantissa2;
+	sc_lv<1>  sign2;
 
 	sc_lv<11> exp_tot;
 	sc_lv<11> mantissa_tot;
+	sc_lv<1>  sign_tot;
+
+	sc_lv<64> result_tot;
 
 	if (reset.read() == 0){
 		STATUS = SR;
@@ -45,50 +50,75 @@ void mul_RTL :: elaborate_mul_FSM(void){
 			exp1 = (number_a.read().range(1,12));
 			exp2 = (number_b.read().range(1,12));
 
-			mantissa1 = (number_a.read().range(13,64));
-			mantissa2 = (number_b.read().range(13,64));
+			mantissa1 = (number_a.read().range(13,63));
+			mantissa2 = (number_b.read().range(13,63));
+
+			sign1 = number_a.read().range(0,1);
+			sign2 = number_b.read().range(0,1);
 
 			break;
 
 			//sommo gli esponenti
 		case S1:
-			//exp_tot = exp1 + exp2;
+			exp_tot = static_cast< sc_uint<11> >( exp1 ) + static_cast< sc_uint<11> >( exp2 );
 
 			break;
 
 			//polarizzo il risultato
 		case S2:
-			//exp_tot = exp_tot -1023;
+			exp_tot = static_cast< sc_uint<11> >( exp_tot ) - 1023;
 
 			break;
 
 			//prodotto tra mantisse
 		case S3:
+			mantissa_tot = static_cast< sc_uint<53> >( mantissa1 ) * static_cast< sc_uint<53> >( mantissa2 );
 
 			break;
 
 			//normalizzo il risultato
 		case S4:
+			if(mantissa_tot[53] != '1'){
+				mantissa_tot << 1;
+				exp_tot = static_cast< sc_uint<11> >( exp_tot )++;
+			}
 
 			break;
 
+			//overflow?
 		case S5:
+			if(static_cast< sc_uint<11> >( exp_tot ) == 0 || static_cast< sc_uint<53> >( mantissa_tot ) == 0)
+				error.write(true);
 
 			break;
 
+			//?
 		case S6:
 
 			break;
 
+			//devo normalizzare ancora?
 		case S7:
+			if(mantissa_tot[0] == '1')
+				normalizzato.write( true );
+			else
+				normalizzato.write( false );
 
 			break;
 
+			//calcolo del segno
 		case S8:
+			sign_tot = sign1 ^ sign2;
 
 			break;
 
 		case S9:
+
+			result_tot.range(0,1) = sign_tot ;
+			result_tot.range(1,12) = exp_tot ;
+			result_tot.range(13,63) = mantissa_tot ;
+
+			vcl_result.write(result_tot);
 
 			break;
 
